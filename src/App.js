31886325e5d4ae9de380d71b00cbc115d5a05e19
amatsukixprog <amatsukixprog@gmail.com>
2019-12-06 02:24:11 +0900

    create class component and fanctional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return(
-    <React.Fragment>
-      <label htmlFor="bar">bbaa</label>
-      <input type="text" onChange={() => {console.log("Iam clicked")}} />
-    </React.Fragment>
+// function App() {
+//   return(
+//     <React.Fragment>
+//       <label htmlFor="bar">bbaa</label>
+//       <input type="text" onChange={() => {console.log("Iam clicked")}} />
+//     </React.Fragment>
+//   )
+// }
+
+// クラスコンポーネンt
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>helloo</div>
+//     )
+//   }
+// }
+
+// ファンクショナルコンポーネント
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
   )
+}
+
+const Cat = () => {
+  return <div>Meow!!</div>
 }
 
 export default App;
